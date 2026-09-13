e58b6ab058c8ac663492a6f380990a7acf3b6bab
Caio Ferreira <caiorcferreira@gmail.com>
2026-09-13 10:10:07 -0300

    feat(cli): add workbench commands

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,27 +1,27 @@
 #!/usr/bin/env bun
-import { Command } from "@effect/cli"
-import { BunContext, BunRuntime } from "@effect/platform-bun"
-import { Console, Effect } from "effect"
-import { setupCommand } from "./commands/setup.js"
-import { workbenchCommand } from "./commands/workbench.js"
+import { Command } from "@effect/cli";
+import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { Console, Effect } from "effect";
+import { setupCommand } from "./commands/setup.js";
+import { workbenchCommand } from "./commands/workbench.js";
 
 const rootCommand = Command.make("hamilton", {}, () =>
-  Console.log("Hamilton - Template setup CLI\n\nUse --help for available commands")
-).pipe(
-  Command.withSubcommands([setupCommand, workbenchCommand])
-)
+  Console.log(
+    "Hamilton - Template setup CLI\n\nUse --help for available commands",
+  ),
+).pipe(Command.withSubcommands([setupCommand, workbenchCommand]));
 
 const cli = Command.run(rootCommand, {
   name: "Hamilton",
-  version: "0.3.0"
-})
+  version: "0.3.0",
+});
 
 cli(process.argv).pipe(
   Effect.catchAll(() =>
     Effect.sync(() => {
-      process.exitCode = 2
-    })
+      process.exitCode = 2;
+    }),
   ),
   Effect.provide(BunContext.layer),
-  BunRuntime.runMain
-)
+  BunRuntime.runMain,
+);
